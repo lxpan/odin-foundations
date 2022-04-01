20bfd876e7164aa131f20b24bc7f7d04cf583d33
Luoxi Pan <l.panpax@gmail.com>
2022-04-01 21:17:59 +1100

    Replace arrow functions with declared functions

--- js-foundations/events/script.js
+++ js-foundations/events/script.js
@@ -5,10 +5,14 @@ btn.onclick = () => alert("Hello World");
 
 // Method 3 - using event listeners
 const btn = document.querySelector('#btn');
-btn.addEventListener('click', () => {
-    alert("Hello World");
-});
+btn.addEventListener('click', alertFunction);
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', consoleLogFunction);
+
+function alertFunction() {
+    alert("Hello Foo and Bar!");
+}
+
+function consoleLogFunction() {
     console.log("Second onclick property");
-});
+}
