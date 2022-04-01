74e726f368caaed950124e012537f6d1b19cb0bc
Luoxi Pan <l.panpax@gmail.com>
2022-04-01 21:14:49 +1100

    Method 3: using event listeners

--- js-foundations/events/script.js
+++ js-foundations/events/script.js
@@ -1,2 +1,14 @@
+/* Method 2
 const btn = document.querySelector('#btn');
 btn.onclick = () => alert("Hello World");
+*/
+
+// Method 3 - using event listeners
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    alert("Hello World");
+});
+
+btn.addEventListener('click', () => {
+    console.log("Second onclick property");
+});
