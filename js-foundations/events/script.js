40b4f07e4882c4b01b18e402d54b3d8447a5bf10
Luoxi Pan <l.panpax@gmail.com>
2022-04-02 16:26:41 +1100

    Add reference to event itself

--- js-foundations/events/script.js
+++ js-foundations/events/script.js
@@ -5,8 +5,8 @@ btn.onclick = () => alert("Hello World");
 
 // Method 3 - using event listeners
 const btn = document.querySelector('#btn');
+const _btn = document.querySelector('#_btn');
 btn.addEventListener('click', alertFunction);
-
 btn.addEventListener('click', consoleLogFunction);
 
 function alertFunction() {
@@ -16,3 +16,8 @@ function alertFunction() {
 function consoleLogFunction() {
     console.log("Second onclick property");
 }
+
+// logs the event (PointerEvent)
+_btn.addEventListener('click', function(e) {
+    console.log(e);
+});
